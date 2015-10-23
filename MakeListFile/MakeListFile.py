7b4038d36e2eb945d0719b3d04d35f5584bf8daa
saya Katafuchi <sk@reiji1020.info>
2015-10-23 12:39:29 +0900

    ちゃんと書いてるのに動かないしキレそう

--- MakeListFile/MakeListFile.py
+++ MakeListFile/MakeListFile.py
@@ -1,4 +1,17 @@
-from PIL import Image
-import os
+import sys
 import glob
 
+# データの入っているファイルを指定
+positive_dir = glob.glob('F:\\IMAGE\\CellDeep\\ImageData_cutting_Positive\\*.*')
+negative_dir = glob.glob('F:\\IMAGE\\CellDeep\\ImageData_cutting_Negative\\*.*')
+
+# ファイルオープン
+listfile = open('F:\\IMAGE\\CellDeep\\train.txt','w')
+
+for pis_file in positive_dir:
+    listfile.write(pis_file+'\n')
+
+for nega_file in negative_dir:
+    listfile.write(nega_file+'\n')
+
+listfile.close()
